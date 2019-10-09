2a94b4c0ba767f26ead852dfc9d253222ac9453b
Jesús Arturo Escobedo Cabello <arturoescobedo.iq@gmail.com>
2019-10-09 14:13:09 -0500

    Change the simple_int_publisher.py to use classes

--- scripts/simple_int_publisher.py
+++ scripts/simple_int_publisher.py
@@ -5,19 +5,25 @@
 import rospy
 from std_msgs.msg import Int32
 
+class SimplePublisher(): 
+    def __init__(self): 
+        print "SimplePublisher init"
+        pub = rospy.Publisher('number_topic', Int32, queue_size=10)
+        rate = rospy.Rate(10) # 10hz
+        number = 0
+        while not rospy.is_shutdown():
+            pub.publish(number)
+            number+=1
+            if number >= 255:
+                number=0
+            rate.sleep()
+    
+    
 
-def talker():
-    rospy.init_node('simple_int_publisher', anonymous=True)
-    pub = rospy.Publisher('number_topic', Int32, queue_size=10)
-    rate = rospy.Rate(10) # 10hz
-    number = 0
-    while not rospy.is_shutdown():
-        pub.publish(number)
-        number+=1
-        if number >= 255:
-            number=0
-        rate.sleep()
-
-if __name__ == '__main__':
-    talker()
-
+################  MAIN PROGRAM ###################### 
+if __name__ == "__main__":
+    rospy.init_node('simple_int_publisher', anonymous=True) 
+    try: 
+        SimplePublisher() 
+    except: 
+        rospy.logfatal("simple_int_publisher died") 
