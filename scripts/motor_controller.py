7452ff32cf73d0ed2a72964181da527784996407
Jesús Arturo Escobedo Cabello <arturoescobedo.iq@gmail.com>
2019-10-27 11:36:52 -0600

    Changing motor_controller and simple_int_publisher to avoid using classes

--- scripts/motor_controller.py
+++ scripts/motor_controller.py
@@ -14,57 +14,48 @@ import rospy
 from std_msgs.msg import Int32
 from std_msgs.msg import String
 
-
-class MotorController():
-    def __init__(self):
-        ############ CONSTANTS ################
-        self.max_speed = 255 #Maximum speed you could change the maximum possible valu from 0 to 255
-        self.velocity_value={'i':self.max_speed,'k':0,',':self.max_speed*-1}
-        print self.velocity_value
-        self.motor_vel = Int32() #Motor velocity ROS message
-        ###******* INIT PUBLISHERS *******###
-        print("Setting publishers...")
-        ##  pub = rospy.Publisher('setPoint', UInt16MultiArray, queue_size=1)
-        self.pub_motor_vel = rospy.Publisher('arduino/motor_vel', Int32, queue_size=1)
-        print("Publishers ok")
-        print("Starting Node...")
-
-        ############################### SUBSCRIBERS #####################################
-        rospy.Subscriber("pressed_key", String, self.pressed_key_cb)
-        rospy.on_shutdown(self.cleanup) #This function will be executed before the node dies
+#### GLOBAL VARIABLES ####
+motor_vel = Int32() #Motor velocity Int32 ROS message
 
 
-        r = rospy.Rate(10) #rate in Hz
-        print("Node initialized")
+def pressed_key_cb(key_msg):
+    global motor_vel
+    max_speed = 255 #Maximum speed: you could change the maximum possible value from 0 to 255
+    velocity_value={'i':max_speed,'k':0,',':-1*max_speed}
+    #This function is executed when a new message arrives to the pressed key topic
+    if key_msg.data in velocity_value.keys():
+        motor_vel.data=velocity_value[key_msg.data]
+    else:
+        print("Not valid key was pressed. Motor will stop")
+        motor_vel.data=0
+    print(motor_vel)
 
-        while not rospy.is_shutdown():
-            self.pub_motor_vel.publish(self.motor_vel) #publish the number
-            r.sleep()
+def MotorController():
+    #########Init the node ################
+    rospy.init_node("motor_controller", anonymous=True)
+    print("Node initialized")
+    ############ CONSTANTS ################
+    global motor_vel
+    ####    PUBLISHERS    ####
+    print("Setting publishers...")
+    pub_motor_vel = rospy.Publisher('arduino/motor_vel', Int32, queue_size=1)
+    print("Publishers ok")
+    print("Starting Node...")
 
-    def pressed_key_cb(self, key):
-        #This function is executed when a new message arrives to the pressed key topic
-        if key.data in self.velocity_value.keys():
-            self.motor_vel.data=self.velocity_value[key.data]
-        else:
-            print("not valid key was sent motor will stop")
-            self.motor_vel.data=0
-        print(self.motor_vel)
+    ####    SUBSCRIBERS    ####
+    rospy.Subscriber("pressed_key", String, pressed_key_cb)
+    r = rospy.Rate(10) #rate in Hz
+    
+    while not rospy.is_shutdown():
+        pub_motor_vel.publish(motor_vel) #publish the number
+        r.sleep()
 
 
-
-    def cleanup(self):
-        #This function is called just before finishing the node
-        # You can use it to clean things up before leaving
-        # Example: stop the robot before finishing a node.
-        vel=Int32()
-        vel.data=0
-        self.pub_motor_vel.publish(vel)
 
 
 ############################### MAIN PROGRAM ####################################
 
 if __name__ == "__main__":
-    rospy.init_node("motor_controller", anonymous=True)
     try:
         MotorController()
     except:
--- scripts/simple_int_publisher.py
+++ scripts/simple_int_publisher.py
@@ -1,29 +1,27 @@
 #!/usr/bin/env python
 """ This program publishes a given integer to a ROS topic
-    The ROS topic is called number_topic
+    published topic:
+        /number_topic
 """
 import rospy
 from std_msgs.msg import Int32
 
-class SimplePublisher(): 
-    def __init__(self): 
-        print "SimplePublisher init"
-        pub = rospy.Publisher('number_topic', Int32, queue_size=10)
-        rate = rospy.Rate(10) # 10hz
-        number = 0
-        while not rospy.is_shutdown():
-            pub.publish(number)
-            number+=1
-            if number >= 255:
-                number=0
-            rate.sleep()
-    
-    
+def simple_publisher():
+    rospy.init_node('simple_int_publisher', anonymous=True)  
+    print("SimplePublisher initialized")
+    pub = rospy.Publisher('number_topic', Int32, queue_size=10)
+    rate = rospy.Rate(10) # 10hz
+    number = 0
+    while not rospy.is_shutdown():
+        pub.publish(number)
+        number+=1
+        if number >= 255:
+            number=0
+        rate.sleep()
 
 ################  MAIN PROGRAM ###################### 
 if __name__ == "__main__":
-    rospy.init_node('simple_int_publisher', anonymous=True) 
     try: 
-        SimplePublisher() 
+        simple_publisher() 
     except: 
         rospy.logfatal("simple_int_publisher died") 
